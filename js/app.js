6deb97703245b31dea2ad79f3322b19a32cfa79c
ecuber <37519236+ecuber@users.noreply.github.com>
2020-01-02 22:30:16 -0500

    Added scrolling and dragging.
    A bit wonky once you scroll out enough, whoops

--- js/app.js
+++ js/app.js
@@ -1,23 +1,38 @@
 const colors = [[31, 33, 61], [64, 74, 140], [104, 109, 183], [111, 115, 168], [176, 182, 242]];
-const multiplier = 15;
-
+let multiplier = 15;
 const DOWN = 0, BR = 0;
 const RIGHT = 1, TR = 1;
 const UP = 2, TL = 2;
 const LEFT = 3, BL = 3;
-let boxes = [];
+
+const controls = {
+  view: {x: 0, y: 0, zoom: 1},
+  viewPos: { prevX: null,  prevY: null,  isDragging: false },
+}
 
 // sets up canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noLoop();
   angleMode(DEGREES);
+  
 }
 
 /**
  * draw - main loop from p5 library.
  */
 function draw() {
+  let boxes = [];
+  
+  // ZOOM AND TRANSLATION
+  translate(controls.view.x, controls.view.y);
+  console.log(controls.view.zoom);
+  if (controls.view.zoom > -0.05) {
+    scale(controls.view.zoom)
+  } else {
+    scale(1);
+  }
+  
+
   background(247, 247, 242);
   stroke(208, 209, 221);
   strokeWeight(2);
@@ -28,7 +43,7 @@ function draw() {
   let y = 450;
   let dir;
 
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < 100; i++) {
     // determines side length in pixels by multiplying the fibonacci value by the constant.
     let len = fibonacci(i) * multiplier;
 
@@ -123,4 +138,84 @@ function fibonacci(index) {
         }
     }
     return arr[index];
+}
+
+/*
+ *
+ * EVENT HANDLERS & CONTROLS CLASS
+ * Written by Amir Saboury: https://codepen.io/amir-s/pen/jzqZdG?editors=0010
+ *
+ */
+
+window.mousePressed = e => Controls.move(controls).mousePressed(e)
+window.mouseDragged = e => Controls.move(controls).mouseDragged(e);
+window.mouseReleased = e => Controls.move(controls).mouseReleased(e)
+
+function mouseWheel(event) {
+  Controls.zoom(controls).worldZoom(event)
+}
+
+
+class Controls {
+  static move(controls) {
+    function mousePressed(e) {
+      controls.viewPos.isDragging = true;
+      controls.viewPos.prevX = e.clientX;
+      controls.viewPos.prevY = e.clientY;
+    }
+
+    function mouseDragged(e) {
+      const {prevX, prevY, isDragging} = controls.viewPos;
+      if(!isDragging) return;
+
+      const pos = {x: e.clientX, y: e.clientY};
+      const dx = pos.x - prevX;
+      const dy = pos.y - prevY;
+
+      if(prevX || prevY) {
+        controls.view.x += dx;
+        controls.view.y += dy;
+        controls.viewPos.prevX = pos.x, controls.viewPos.prevY = pos.y
+      }
+    }
+
+    function mouseReleased(e) {
+      controls.viewPos.isDragging = false;
+      controls.viewPos.prevX = null;
+      controls.viewPos.prevY = null;
+    }
+ 
+    return {
+      mousePressed, 
+      mouseDragged, 
+      mouseReleased
+    }
+  }
+
+  static zoom(controls) {
+    // function calcPos(x, y, zoom) {
+    //   const newX = width - (width * zoom - x);
+    //   const newY = height - (height * zoom - y);
+    //   return {x: newX, y: newY}
+    // }
+
+    function worldZoom(e) {
+      const {x, y, deltaY} = e;
+      const direction = deltaY < 0 ? -1 : 1;
+      const factor = 0.05;
+      
+      const zoom = direction * factor;
+    
+      const wx = (x-controls.view.x)/(width*controls.view.zoom);
+      const wy = (y-controls.view.y)/(height*controls.view.zoom);
+      
+      controls.view.x -= wx*width*zoom;
+      controls.view.y -= wy*height*zoom;
+      controls.view.zoom += zoom;
+      
+      
+    }
+
+    return {worldZoom}
+  }
 }
